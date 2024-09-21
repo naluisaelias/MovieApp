01eb5bb85428ac288637de0d60e2dd49643b4711
Ana Luísa <naluisaelias@gmail.com>
2024-09-21 09:11:15 -0300

    API: Definição de idioma

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -7,65 +7,66 @@ import 'package:movie_app/models/movie_model.dart';
 
 const baseUrl = 'https://api.themoviedb.org/3/';
 const key = '?api_key=$apiKey';
+const language = '&language=pt-BR'; // Add: Variável para definição de idioma
 
 class ApiServices {
   Future<Result> getTopRatedMovies() async {
     var endPoint = 'movie/top_rated';
-    final url = '$baseUrl$endPoint$key';
+    final url = '$baseUrl$endPoint$key$language'; // Add: Definição de idioma
 
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       return Result.fromJson(jsonDecode(response.body));
     }
-    throw Exception('failed to load now playing movies');
+    throw Exception('Falha ao carregar filmes mais bem avaliados'); // Update: Mensagem no idioma da API
   }
 
   Future<Result> getNowPlayingMovies() async {
     var endPoint = 'movie/now_playing';
-    final url = '$baseUrl$endPoint$key';
+    final url = '$baseUrl$endPoint$key$language'; // Add: Definição de idioma
 
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       return Result.fromJson(jsonDecode(response.body));
     }
-    throw Exception('failed to load now playing movies');
+    throw Exception('Falha ao carregar filmes em cartaz'); // Update: Mensagem no idioma da API
   }
 
   Future<Result> getUpcomingMovies() async {
     var endPoint = 'movie/upcoming';
-    final url = '$baseUrl$endPoint$key';
+    final url = '$baseUrl$endPoint$key$language'; // Add: Definição de idioma
 
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       return Result.fromJson(jsonDecode(response.body));
     }
-    throw Exception('failed to load upcoming movies');
+    throw Exception('Falha ao carregar próximos lançamentos'); // Update: Mensagem no idioma da API
   }
 
   Future<MovieDetailModel> getMovieDetail(int movieId) async {
     final endPoint = 'movie/$movieId';
-    final url = '$baseUrl$endPoint$key';
+    final url = '$baseUrl$endPoint$key$language'; // Add: Definição de idioma
 
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       return MovieDetailModel.fromJson(jsonDecode(response.body));
     }
-    throw Exception('failed to load  movie details');
+    throw Exception('Falha ao carregar detalhes do filme'); // Update: Mensagem no idioma da API
   }
 
   Future<Result> getMovieRecommendations(int movieId) async {
-    final endPoint = 'movie/$movieId/recommendations';
-    final url = '$baseUrl$endPoint$key';
+    final endPoint = 'movie/$movieId/recommendations'; // Add: Definição de idioma
+    final url = '$baseUrl$endPoint$key$language';
 
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       return Result.fromJson(jsonDecode(response.body));
     }
-    throw Exception('failed to load  movie details');
+    throw Exception('Falha ao carregar recomendações de filmes'); // Update: Mensagem no idioma da API
   }
 
   Future<Result> getSearchedMovie(String searchText) async {
-    final endPoint = 'search/movie?query=$searchText';
+    final endPoint = 'search/movie?query=$searchText$language'; // Add: Definição de idioma
     final url = '$baseUrl$endPoint';
     final response = await http.get(Uri.parse(url), headers: {
       'Authorization':
@@ -75,17 +76,17 @@ class ApiServices {
       final movies = Result.fromJson(jsonDecode(response.body));
       return movies;
     }
-    throw Exception('failed to load  search movie ');
+    throw Exception('Falha ao carregar filme pesquisado'); // Update: Mensagem no idioma da API
   }
 
   Future<Result> getPopularMovies() async {
     const endPoint = 'movie/popular';
-    const url = '$baseUrl$endPoint$key';
+    final url = '$baseUrl$endPoint$key$language'; // Add: Definição de idioma
 
-    final response = await http.get(Uri.parse(url), headers: {});
+    final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       return Result.fromJson(jsonDecode(response.body));
     }
-    throw Exception('failed to load now playing movies');
+    throw Exception('Falha ao carregar filmes populares'); // Update: Mensagem no idioma da API
   }
 }
